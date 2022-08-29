87246ad938a988f710db9b839c5bcf69959f24a6
Branko Fibinger <branko.fibinger1@gmail.com>
2022-08-29 22:52:51 +0200

    Add project files.

---
+++ LanGuideBrankoFibinger/LanGuideBrankoFibinger/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using LanGuideBrankoFibinger.Helpers;
+
+namespace LanGuideBrankoFibinger
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void loginButton_Clicked(object sender, EventArgs e)
+        {
+            bool isEmailEmpty = string.IsNullOrEmpty(emailEntry.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(passEntry.Text);
+            if (!isEmailEmpty && !isPasswordEmpty)
+            {
+                bool result = await Auth.LoginUser(emailEntry.Text, passEntry.Text);
+                if (result) await Navigation.PushAsync(new HomePage());
+            }
+            else
+            {
+                // do not navigate
+            }
+        }
+    }
+}
